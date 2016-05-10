585d56161a6fcbd92cb6e2bb7b0d774163894e65
Jörn Franke <jornfranke@gmail.com>
2016-05-10 23:03:26 +0200

    Added example how to use standard Hive functions to check for Bitcoin transactions to a certain Bitcoin address

--- examples/hive-bitcoin/hive-bitcoin.sql
+++ examples/hive-bitcoin/hive-bitcoin.sql
@@ -4,7 +4,7 @@ create database blockchains;
 use blockchains;
 
 -- create table representing Bitcoin Blockchain data stored in /user/cloudera/bitcoin/input
-Create table BitcoinBlockchain ROW FORMAT SERDE 'org.zuinnote.hadoop.bitcoin.hive.serde.BitcoinBlockSerde' STORED AS INPUTFORMAT 'org.zuinnote.hadoop.bitcoin.format.BitcoinBlockFileInputFormat' OUTPUTFORMAT 'org.apache.hadoop.mapreduce.lib.output.NullOutputFormat' LOCATION '/user/cloudera/bitcoin/input';
+create table BitcoinBlockchain ROW FORMAT SERDE 'org.zuinnote.hadoop.bitcoin.hive.serde.BitcoinBlockSerde' STORED AS INPUTFORMAT 'org.zuinnote.hadoop.bitcoin.format.BitcoinBlockFileInputFormat' OUTPUTFORMAT 'org.apache.hadoop.mapreduce.lib.output.NullOutputFormat' LOCATION '/user/cloudera/bitcoin/input';
 
 -- The following example counts the number of blocks:
 
@@ -15,4 +15,8 @@ select count(*) from BitcoinBlockchain LATERAL VIEW explode(transactions) explod
 
 -- The following example calculates the total output of all Bitcoin transactions
 select SUM(expout.value) FROM (select * from BitcoinBlockchain LATERAL VIEW explode(transactions) exploded_transactions as exptran) transaction_table LATERAL VIEW explode(exptran.listofoutputs) exploded_outputs as expout;
+
+-- The following example gets the number of transaction outputs to the Bitocoin address C825A1ECF2A6830C4401620C3A16F1995057C2AB (please keep in mind that this is the raw address used in Bitcoin, find out how to convert it from a user friendly here: https://en.bitcoin.it/wiki/List_of_address_prefixes)
+-- please note that this covers only transaction using the Pay to hash mechanism: https://en.bitcoin.it/wiki/Pay_to_script_hash
+select count(*) FROM (select * from BitcoinBlockchain LATERAL VIEW explode(transactions) exploded_transactions as exptran) transaction_table LATERAL VIEW explode(exptran.listofoutputs) exploded_outputs as expout WHERE instr(substring(regexp_extract(hex(expout.txoutscript),"76A914.*88AC",0),7,41),"C825A1ECF2A6830C4401620C3A16F1995057C2AB")>0;
 
