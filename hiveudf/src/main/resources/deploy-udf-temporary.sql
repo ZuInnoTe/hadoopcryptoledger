1bbb75257704ba29c39b8429137b5904951bdc7b
Jörn Franke <jornfranke@gmail.com>
2016-06-25 01:41:59 +0200

    Added deployment scripts

---
+++ hiveudf/src/main/resources/deploy-udf-temporary.sql
@@ -0,0 +1,2 @@
+CREATE TEMPORARY FUNCTION hclBitcoinTransactionHash as 'org.zuinnote.hadoop.bitcoin.hive.udf.BitcoinTransactionHashUDF' USING JAR '/tmp/hadoopcryptoledger-hiveudf-1.0.0.jar';
+CREATE TEMPORARY FUNCTION hclBitcoinScriptPattern as 'org.zuinnote.hadoop.bitcoin.hive.udf.BitcoinScriptPaymentPatternAnalyzerUDF' USING JAR '/tmp/hadoopcryptoledger-hiveudf-1.0.0.jar';
