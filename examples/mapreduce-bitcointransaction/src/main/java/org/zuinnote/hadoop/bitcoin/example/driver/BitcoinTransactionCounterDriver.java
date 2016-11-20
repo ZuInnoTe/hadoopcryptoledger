28c93e756ca6bae79dd919e8b88a05ee10e30701
Jörn Franke <jornfranke@gmail.com>
2016-11-20 17:29:43 +0100

    Fixed some Sonar issues

--- examples/mapreduce-bitcointransaction/src/main/java/org/zuinnote/hadoop/bitcoin/example/driver/BitcoinTransactionCounterDriver.java
+++ examples/mapreduce-bitcointransaction/src/main/java/org/zuinnote/hadoop/bitcoin/example/driver/BitcoinTransactionCounterDriver.java
@@ -39,7 +39,8 @@ import org.zuinnote.hadoop.bitcoin.format.*;
 
 public class BitcoinTransactionCounterDriver  {
 
-       
+ private BitcoinTransactionCounterDriver() {
+ }
         
  public static void main(String[] args) throws Exception {
     JobConf conf = new JobConf(BitcoinTransactionCounterDriver.class);
