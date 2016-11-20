bdea16501d825445091eff4f5f79687acd8afb09
Jörn Franke <jornfranke@gmail.com>
2016-11-20 17:35:55 +0100

    Fixed some Sonar issues

--- inputformat/src/main/java/org/zuinnote/hadoop/bitcoin/format/BitcoinTransactionFileInputFormat.java
+++ inputformat/src/main/java/org/zuinnote/hadoop/bitcoin/format/BitcoinTransactionFileInputFormat.java
@@ -13,7 +13,6 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 **/
-
 package org.zuinnote.hadoop.bitcoin.format;
 
 import org.zuinnote.hadoop.bitcoin.format.exception.HadoopCryptoLedgerConfigurationException;
