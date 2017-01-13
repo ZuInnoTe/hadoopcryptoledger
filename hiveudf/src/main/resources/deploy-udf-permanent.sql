476c972f1c8f9970347bead07f97969e466714dd
Jörn Franke <jornfranke@gmail.com>
2017-01-13 21:29:25 +0100

    removed

--- hiveudf/src/main/resources/deploy-udf-permanent.sql
+++
@@ -1,4 +0,0 @@
-CREATE database IF NOT EXISTS hcl;
-USE hcl;
-CREATE FUNCTION hclBitcoinTransactionHash as 'org.zuinnote.hadoop.bitcoin.hive.udf.BitcoinTransactionHashUDF' USING JAR '/tmp/hadoopcryptoledger-hiveudf-1.0.3.jar';
-CREATE FUNCTION hclBitcoinScriptPattern as 'org.zuinnote.hadoop.bitcoin.hive.udf.BitcoinScriptPaymentPatternAnalyzerUDF' USING JAR '/tmp/hadoopcryptoledger-hiveudf-1.0.3.jar';
