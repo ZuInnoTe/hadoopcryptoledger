10e077fda29e14a7baa983b5aeeee740f67665e3
Jörn Franke <jornfranke@gmail.com>
2017-02-01 22:38:29 +0100

    Added test

--- inputformat/src/test/java/org/zuinnote/hadoop/bitcoin/format/common/BitcoinScriptPatternParserTest.java
+++ inputformat/src/test/java/org/zuinnote/hadoop/bitcoin/format/common/BitcoinScriptPatternParserTest.java
@@ -32,10 +32,19 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.security.NoSuchAlgorithmException;
 
-import org.zuinnote.hadoop.bitcoin.format.common.BitcoinUtil;
+import org.zuinnote.hadoop.bitcoin.format.common.BitcoinScriptPatternParser;
 
 
 public class BitcoinScriptPatternParserTest {
+
+@Test
+  public void testPaymentPubKeyGenesis() {
+	byte[] txOutScriptGenesis= new byte[]{(byte)0x41,(byte)0x04,(byte)0x67,(byte)0x8A,(byte)0xFD,(byte)0xB0,(byte)0xFE,(byte)0x55,(byte)0x48,(byte)0x27,(byte)0x19,(byte)0x67,(byte)0xF1,(byte)0xA6,(byte)0x71,(byte)0x30,(byte)0xB7,(byte)0x10,(byte)0x5C,(byte)0xD6,(byte)0xA8,(byte)0x28,(byte)0xE0,(byte)0x39,(byte)0x09,(byte)0xA6,(byte)0x79,(byte)0x62,(byte)0xE0,(byte)0xEA,(byte)0x1F,(byte)0x61,(byte)0xDE,(byte)0xB6,(byte)0x49,(byte)0xF6,(byte)0xBC,(byte)0x3F,(byte)0x4C,(byte)0xEF,(byte)0x38,(byte)0xC4,(byte)0xF3,(byte)0x55,(byte)0x04,(byte)0xE5,(byte)0x1E,(byte)0xC1,(byte)0x12,(byte)0xDE,(byte)0x5C,(byte)0x38,(byte)0x4D,(byte)0xF7,(byte)0xBA,(byte)0x0B,(byte)0x8D,(byte)0x57,(byte)0x8A,(byte)0x4C,(byte)0x70,(byte)0x2B,(byte)0x6B,(byte)0xF1,(byte)0x1D,(byte)0x5F,(byte)0xAC};
+       String result = BitcoinScriptPatternParser.getPaymentDestination(txOutScriptGenesis);
+	String comparatorText = "bitcoinpubkey_4104678AFDB0FE5548271967F1A67130B7105CD6A828E03909A67962E0EA1F61DEB649F6BC3F4CEF38C4F35504E51EC112DE5C384DF7BA0B8D578A4C702B6BF11D5F";
+	assertEquals("TxOutScript from Genesis should be payment to a pubkey address", comparatorText,result);
+  }
+
 
 }
 
