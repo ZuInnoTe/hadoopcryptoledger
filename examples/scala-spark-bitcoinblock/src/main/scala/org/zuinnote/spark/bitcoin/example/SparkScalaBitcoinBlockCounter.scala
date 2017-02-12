f16fd1c7fffc6c56aea06465cb91bdc4bd1da143
Jörn Franke <jornfranke@gmail.com>
2017-02-13 00:13:02 +0100

    refactored to facilitate testing

--- examples/scala-spark-bitcoinblock/src/main/scala/org/zuinnote/spark/bitcoin/example/SparkScalaBitcoinBlockCounter.scala
+++ examples/scala-spark-bitcoinblock/src/main/scala/org/zuinnote/spark/bitcoin/example/SparkScalaBitcoinBlockCounter.scala
@@ -17,6 +17,7 @@ package org.zuinnote.spark.bitcoin.example
 
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
 import org.apache.hadoop.conf._
 
 import org.apache.hadoop.fs.Path
@@ -36,16 +37,28 @@ object SparkScalaBitcoinBlockCounter {
         val conf = new SparkConf().setAppName("Spark-Scala BitcoinBlock Analytics (hadoopcryptoledger)")
 	val sc=new SparkContext(conf)
 	val hadoopConf = new Configuration()
-	 hadoopConf.set("hadoopcryptoledger.bitcoinblockinputformat.filter.magic","F9BEB4D9");
-	val bitcoinBlocksRDD = sc.newAPIHadoopFile(args(0), classOf[BitcoinBlockFileInputFormat], classOf[BytesWritable], classOf[BitcoinBlock], hadoopConf);
-	// extract the no transactions / block (map)
-   	val noOfTransactionPair = bitcoinBlocksRDD.map(hadoopKeyValueTuple => ("No of transactions: ",hadoopKeyValueTuple._2.getTransactions().size()));
-	// reduce total count
-	val totalCount = noOfTransactionPair.reduceByKey ((c,d) => c+d);
-
-    	// write results to HDFS
-	totalCount.repartition(1).saveAsTextFile(args(1));
+	 hadoopConf.set("hadoopcryptoledger.bitcoinblockinputformat.filter.magic","F9BEB4D9")
+	jobTotalNumOfTransactions(sc,hadoopConf,args(0),args(1))
+	sc.stop()
       }
-    }
+
+
+     def jobTotalNumOfTransactions(sc: SparkContext, hadoopConf: Configuration, inputFile: String, outputFile: String): Unit = {
+	val bitcoinBlocksRDD = sc.newAPIHadoopFile(inputFile, classOf[BitcoinBlockFileInputFormat], classOf[BytesWritable], classOf[BitcoinBlock], hadoopConf)
+	val totalCount=transform(bitcoinBlocksRDD)
+    	// write results to HDFS
+	totalCount.repartition(1).saveAsTextFile(outputFile)
+	
+	}
+
+	def transform(bitcoinBlocksRDD: RDD[(BytesWritable,BitcoinBlock)]): RDD[(String,Int)] = {
+		// extract the no transactions / block (map)
+   		val noOfTransactionPair = bitcoinBlocksRDD.map(hadoopKeyValueTuple => ("No of transactions: ",hadoopKeyValueTuple._2.getTransactions().size()))
+		// reduce total count
+		val totalCount = noOfTransactionPair.reduceByKey ((c,d) => c+d)
+
+		totalCount
+	}
+ }
 
 
