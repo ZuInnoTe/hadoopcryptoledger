63ed9824e613caf0df03f1531c9b8b5c995b6507
Jörn Franke <jornfranke@gmail.com>
2017-09-13 00:00:11 +0200

    added support for identifying paytohash and paytopublickey with segwit infromation

--- inputformat/src/main/java/org/zuinnote/hadoop/bitcoin/format/common/BitcoinScriptPatternParser.java
+++ inputformat/src/main/java/org/zuinnote/hadoop/bitcoin/format/common/BitcoinScriptPatternParser.java
@@ -43,6 +43,16 @@ public static String getPaymentDestination(byte[] scriptPubKey) {
 	if (scriptPubKey.length==0) {
 		return "anyone"; // need to check also ScriptSig for OP_TRUE
 	}
+	// test if standard transaction Bitcoin address including Segwit
+	String payToHashSegwit = checkP2WPKHP2SH(scriptPubKey);
+	if (payToHashSegwit!=null) {
+		return payToHashSegwit;
+	}
+	// test if standard transaction public key including Segwit
+	String payToPubKeySegwit = checkP2WPKH(scriptPubKey);
+	if (payToPubKeySegwit!=null) {
+		return payToPubKeySegwit;
+	}	
 	// test if standard transaction to Bitcoin address
 	String payToHash = checkPayToHash(scriptPubKey);
 	if (payToHash!=null) {
@@ -65,6 +75,47 @@ public static String getPaymentDestination(byte[] scriptPubKey) {
 	return null;
 }
 
+
+/**
+ * Checks if scriptPubKey is about a transaction for pay-to-witness-public-key-hash (P2WPKH)
+ * Note that we return only the keyhash, but more information can be found in the witness (cf. https://github.com/bitcoin/bips/blob/master/bip-0141.mediawiki)
+ * 
+ * 
+ * @param scriptPubKey
+ * @return null, if transaction is not about P2WPKH, a string starting with "P2WPKH_keyhash"
+ */
+
+private static String checkP2WPKH(byte[] scriptPubKey) {
+	if ((scriptPubKey.length==22) && (scriptPubKey[0]==0) && (scriptPubKey[1]==0x14)){
+		byte[] keyhash = Arrays.copyOfRange(scriptPubKey, 2, 22);
+		return "P2WPKH_"+BitcoinUtil.convertByteArrayToHexString(keyhash);
+	}
+	return null;
+}
+
+/**
+ * Checks if scriptPubKey is about a transaction for pay-to-witness-public-key-hash (P2WPKH) nested in BIP16 P2SH
+ * Note that we return only the keyhash, but more information can be found in 
+ * (1) the witness (cf. https://github.com/bitcoin/bips/blob/master/bip-0141.mediawiki)
+ * (2) in scriptSig (keyhash)
+ * 
+ * @param scriptPubKey
+ * @return null, if transaction is not about P2WSH, a string starting with "P2WSH_keyhash"
+ */
+
+private static String checkP2WPKHP2SH(byte[] scriptPubKey) {
+	boolean validLength=scriptPubKey.length==23;
+	if (!(validLength)) {
+		return null;
+	}
+	boolean validStart=((scriptPubKey[0] & 0xFF)==0xA9) && ((scriptPubKey[1] & 0xFF)==0x14);
+	boolean validEnd=(scriptPubKey[22] & 0xFF)==0x87;
+	if (validStart && validEnd){
+		byte[] keyhash = Arrays.copyOfRange(scriptPubKey, 2, 22);
+		return "P2WPKHP2SH_"+BitcoinUtil.convertByteArrayToHexString(keyhash);
+	}
+	return null;
+}
 
 /***
 * Checks if scriptPubKey is about a transaction for paying to a hash
