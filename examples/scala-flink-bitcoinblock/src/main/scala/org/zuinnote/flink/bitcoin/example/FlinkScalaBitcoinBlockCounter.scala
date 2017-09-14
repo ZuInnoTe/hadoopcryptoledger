aeaf0b0cf50c987cf417f122cd5d9795c1de4878
Jörn Franke <jornfranke@gmail.com>
2017-09-14 22:27:22 +0200

    small fix

--- examples/scala-flink-bitcoinblock/src/main/scala/org/zuinnote/flink/bitcoin/example/FlinkScalaBitcoinBlockCounter.scala
+++ examples/scala-flink-bitcoinblock/src/main/scala/org/zuinnote/flink/bitcoin/example/FlinkScalaBitcoinBlockCounter.scala
@@ -21,7 +21,7 @@ import org.apache.flink.core.fs.FileSystem
 
 import org.zuinnote.flink.bitcoin._
 
-import import org.zuinnote.hadoop.bitcoin.format.mapred.AbstractBitcoinRecordReader
+import  org.zuinnote.hadoop.bitcoin.format.mapred.AbstractBitcoinRecordReader
 
 /**
 *
