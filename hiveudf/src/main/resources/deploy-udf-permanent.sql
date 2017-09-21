276aa9edae4d12d7a9363443278c2afa8b19ba06
Jörn Franke <jornfranke@gmail.com>
2017-09-21 20:57:35 +0200

    added hclBitcoinTransactionHashSegwit to deployment

--- hiveudf/src/main/resources/deploy-udf-permanent.sql
+++ hiveudf/src/main/resources/deploy-udf-permanent.sql
@@ -1,4 +1,5 @@
 CREATE database IF NOT EXISTS hcl;
 USE hcl;
 CREATE FUNCTION hclBitcoinTransactionHash as 'org.zuinnote.hadoop.bitcoin.hive.udf.BitcoinTransactionHashUDF' USING JAR '/tmp/hadoopcryptoledger-hiveudf-1.0.7.jar';
+CREATE FUNCTION hclBitcoinTransactionHashSegwit as 'org.zuinnote.hadoop.bitcoin.hive.udf.BitcoinTransactionHashSegwitUDF' USING JAR '/tmp/hadoopcryptoledger-hiveudf-1.0.7.jar';
 CREATE FUNCTION hclBitcoinScriptPattern as 'org.zuinnote.hadoop.bitcoin.hive.udf.BitcoinScriptPaymentPatternAnalyzerUDF' USING JAR '/tmp/hadoopcryptoledger-hiveudf-1.0.7.jar';
--- hiveudf/src/main/resources/deploy-udf-temporary.sql
+++ hiveudf/src/main/resources/deploy-udf-temporary.sql
@@ -1,2 +1,3 @@
 CREATE TEMPORARY FUNCTION hclBitcoinTransactionHash as 'org.zuinnote.hadoop.bitcoin.hive.udf.BitcoinTransactionHashUDF' USING JAR '/tmp/hadoopcryptoledger-hiveudf-1.0.7.jar';
+CREATE TEMPORARY FUNCTION hclBitcoinTransactionHashSegwit as 'org.zuinnote.hadoop.bitcoin.hive.udf.BitcoinTransactionHashSegwitUDF' USING JAR '/tmp/hadoopcryptoledger-hiveudf-1.0.7.jar';
 CREATE TEMPORARY FUNCTION hclBitcoinScriptPattern as 'org.zuinnote.hadoop.bitcoin.hive.udf.BitcoinScriptPaymentPatternAnalyzerUDF' USING JAR '/tmp/hadoopcryptoledger-hiveudf-1.0.7.jar';
