7a34af046396f114676f7db076b10b534e8811f0
Jörn Franke <jornfranke@gmail.com>
2017-11-11 19:10:56 +0100

    updated deployment UDF to new version and added EthereumUDFs

--- hiveudf/src/main/resources/deploy-udf-temporary.sql
+++ hiveudf/src/main/resources/deploy-udf-temporary.sql
@@ -1,5 +1,8 @@
-CREATE TEMPORARY FUNCTION hclBitcoinTransactionHash as 'org.zuinnote.hadoop.bitcoin.hive.udf.BitcoinTransactionHashUDF' USING JAR '/tmp/hadoopcryptoledger-hiveudf-1.0.8.jar';
-CREATE TEMPORARY FUNCTION hclBitcoinTransactionHashSegwit as 'org.zuinnote.hadoop.bitcoin.hive.udf.BitcoinTransactionHashSegwitUDF' USING JAR '/tmp/hadoopcryptoledger-hiveudf-1.0.8.jar';
-CREATE TEMPORARY FUNCTION hclBitcoinScriptPattern as 'org.zuinnote.hadoop.bitcoin.hive.udf.BitcoinScriptPaymentPatternAnalyzerUDF' USING JAR '/tmp/hadoopcryptoledger-hiveudf-1.0.8.jar';
-CREATE TEMPORARY FUNCTION hclNamecoinExtractField as 'org.zuinnote.hadoop.namecoin.hive.udf.NamecoinExtractFieldUDF' USING JAR '/tmp/hadoopcryptoledger-hiveudf-1.0.8.jar';
-CREATE TEMPORARY FUNCTION hclNamecoinGetNameOperation as 'org.zuinnote.hadoop.namecoin.hive.udf.NamecoinGetNameOperationUDF' USING JAR '/tmp/hadoopcryptoledger-hiveudf-1.0.8.jar';
+CREATE TEMPORARY FUNCTION hclBitcoinTransactionHash as 'org.zuinnote.hadoop.bitcoin.hive.udf.BitcoinTransactionHashUDF' USING JAR '/tmp/hadoopcryptoledger-hiveudf-1.1.0.jar';
+CREATE TEMPORARY FUNCTION hclBitcoinTransactionHashSegwit as 'org.zuinnote.hadoop.bitcoin.hive.udf.BitcoinTransactionHashSegwitUDF' USING JAR '/tmp/hadoopcryptoledger-hiveudf-1.1.0.jar';
+CREATE TEMPORARY FUNCTION hclBitcoinScriptPattern as 'org.zuinnote.hadoop.bitcoin.hive.udf.BitcoinScriptPaymentPatternAnalyzerUDF' USING JAR '/tmp/hadoopcryptoledger-hiveudf-1.1.0.jar';
+CREATE TEMPORARY FUNCTION hclNamecoinExtractField as 'org.zuinnote.hadoop.namecoin.hive.udf.NamecoinExtractFieldUDF' USING JAR '/tmp/hadoopcryptoledger-hiveudf-1.1.0.jar';
+CREATE TEMPORARY FUNCTION hclNamecoinGetNameOperation as 'org.zuinnote.hadoop.namecoin.hive.udf.NamecoinGetNameOperationUDF' USING JAR '/tmp/hadoopcryptoledger-hiveudf-1.1.0.jar';
+CREATE TEMPORARY FUNCTION hclEthereumGetChainId as 'org.zuinnote.hadoop.ethereum.hive.udf.EthereumGetChainIdUDF' USING JAR '/tmp/hadoopcryptoledger-hiveudf-1.1.0.jar';
+CREATE TEMPORARY FUNCTION hclEthereumGetSendAddress as 'org.zuinnote.hadoop.ethereum.hive.udf.EthereumGetSendAddressUDF' USING JAR '/tmp/hadoopcryptoledger-hiveudf-1.1.0.jar';
+CREATE TEMPORARY FUNCTION hclEthereumGetTransactionHash as 'org.zuinnote.hadoop.ethereum.hive.udf.EthereumGetTransactionHashUDF' USING JAR '/tmp/hadoopcryptoledger-hiveudf-1.1.0.jar';
