c0fd8c5c3b1298677022a69ec53325ed35ed30be
Steve Phelps <sphelps@sphelps.net>
2020-10-11 19:43:53 +0100

    javadoc

--- inputformat/src/main/java/org/zuinnote/hadoop/bitcoin/format/common/LittleEndianUInt32.java
+++ inputformat/src/main/java/org/zuinnote/hadoop/bitcoin/format/common/LittleEndianUInt32.java
@@ -5,12 +5,19 @@ import java.nio.ByteOrder;
 
 /**
  * An implementation of an unsigned 32-bit integer in Java.
+ *
+ * The value is stored in a raw little-endian four-byte buffer, and then converted
+ * to a 64-bit signed long on-demand.  This allows UINT32 values  to be stored efficiently
+ * for big-data computations while retaining the full range of positive values.
  */
 public class LittleEndianUInt32 extends Number {
 
     public static final int NUM_BYTES = 4;
     public static final long MAX = (1L<<(NUM_BYTES*8)) - 1;
 
+    /**
+     * The raw data stored as 4 bytes in little-endian order.
+     */
     protected ByteBuffer rawData;
 
     public LittleEndianUInt32() {
@@ -24,6 +31,11 @@ public class LittleEndianUInt32 extends Number {
     }
 
     public LittleEndianUInt32(long value) {
+        this();
+        setValue(value);
+    }
+
+    public LittleEndianUInt32(ByteBuffer value) {
         this();
         setValue(value);
     }
@@ -45,10 +57,16 @@ public class LittleEndianUInt32 extends Number {
         rawData.put(value, 0, NUM_BYTES);
     }
 
+    public void setValue(ByteBuffer buffer) {
+        for(int i=0; i<NUM_BYTES; i++) {
+           rawData.put(i, buffer.get());
+        }
+    }
+
     @Override
     public int intValue() {
         int result = (int) getValue();
-        if (result < 0) throw new IllegalArgumentException("unsigned int overflow");
+        if (result < 0) throw new IllegalArgumentException("signed int overflow");
         return result;
     }
 
@@ -66,4 +84,5 @@ public class LittleEndianUInt32 extends Number {
     public double doubleValue() {
         return (double) getValue();
     }
+
 }
